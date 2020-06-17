61441ec3ba8a749e2b554fc4bc26bef46272c6db
Michal Sniezko <michal@getvolt.io>
2020-06-17 13:54:34 +0200

    two_d_translate

---
+++ two_d_translate.rb
@@ -0,0 +1,27 @@
+# Write a method two_d_translate that takes in a 2 dimensional array and translates it into a 1 dimensional array.
+# You can assume that the inner arrays always have 2 elements. See the examples.
+
+def two_d_translate(arr)
+  result = []
+  arr.each do |a|
+    a[1].times{result << a[0]}
+  end
+  result
+end
+
+arr_1 = [
+  ['boot', 3],
+  ['camp', 2],
+  ['program', 0]
+]
+
+print two_d_translate(arr_1) # => [ 'boot', 'boot', 'boot', 'camp', 'camp' ]
+puts
+
+arr_2 = [
+  ['red', 3],
+  ['blue', 4]
+]
+
+print two_d_translate(arr_2) # => [ 'red', 'blue', 'blue', 'blue', 'blue' ]
+puts
